bb231013c724e74b886d96eb9f1b6a5be227fb03
Adrian Cinal <adriancinal@gmail.com>
2022-04-15 18:52:19 +0200

    Add login/logout notifications

--- client/session_manager_client/__init__.py
+++ client/session_manager_client/__init__.py
@@ -16,6 +16,7 @@ from common.messages import (
     cans_recv,
     cans_send,
 )
+from common.messages.messages import CansMessage
 
 
 class SessionManager:
@@ -31,6 +32,14 @@ class SessionManager:
         # TODO: Implement client-side logging
         self.log = logging.getLogger("cans-logger")
 
+        self.message_handlers = {
+            CansMsgId.USER_MESSAGE: self.__handle_message_user,
+            CansMsgId.PEER_UNAVAILABLE: self.__handle_message_peer_unavailable,
+            CansMsgId.PEER_LOGIN: self.__handle_message_peer_login,
+            CansMsgId.PEER_LOGOUT: self.__handle_message_peer_logout,
+            CansMsgId.ACTIVE_FRIENDS: self.__handle_message_active_friends,
+        }
+
     async def connect(self, url: str) -> None:
         """Connect to the server."""
         ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
@@ -40,50 +49,79 @@ class SessionManager:
         )
 
         async with ws.connect(url, ssl=ssl_context) as conn:
-            # TODO: Run the actual client
-            await self.__dummy_poc_client(conn)
+            await asyncio.gather(
+                self.__handle_upstream(conn), self.__handle_downstream(conn)
+            )
 
-    async def __dummy_poc_client(
+    async def __handle_upstream(
         self, conn: ws.WebSocketClientProtocol
     ) -> None:
-        """Run dummy client implementation."""
-        # NOTE: For PoC purposes get a mock public_key
-        # from the command line
+        """Handle upstream traffic, i.e. client to server."""
         public_key = self.key_manager.get_own_public_key_digest()
         peer_key = sys.argv[2]
 
         if "Alice" in public_key:
-            # Test "peer unavailable" condition
+            # Test peer unavailable and login notifications
             await asyncio.sleep(5)
 
         # Say hello to the server
-        hello = ServerHello()
-        hello.payload["public_key"] = public_key
+        hello = ServerHello(public_key, [peer_key])
         await cans_send(hello, conn)
 
         while True:
-            request = self.user_message_to(peer_key)
+            request = self.__user_message_to(peer_key)
             request.payload = f"Hello {peer_key}, this is {public_key}"
             await cans_send(request, conn)
+            await asyncio.sleep(3)
 
-            response = await cans_recv(conn)
+    async def __handle_downstream(
+        self, conn: ws.WebSocketClientProtocol
+    ) -> None:
+        """Handle downstream traffic, i.e. server to client."""
+        while True:
+            message = await cans_recv(conn)
 
-            if response.header.sender != "":
-                print(
-                    f"Received message '{response.payload}' from"
-                    + f" {response.header.sender}"
-                )
-            elif response.header.msg_id == CansMsgId.PEER_UNAVAILABLE:
-                print(f"Peer {peer_key} is unavailable")
+            if message.header.msg_id in self.message_handlers.keys():
+                # Call a registered handler
+                await self.message_handlers[message.header.msg_id](message)
             else:
                 print(
                     "Received unexpected message with ID:"
-                    + f"{response.header.msg_id}"
+                    + f"{message.header.msg_id}"
                 )
 
-            await asyncio.sleep(1)
+    async def __handle_message_user(self, message: CansMessage) -> None:
+        """Handle message type USER_MESSAGE."""
+        print(
+            f"Received user message {message.payload} from"
+            + f" {message.header.sender}"
+        )
 
-    def user_message_to(self, peer: PubKeyDigest) -> UserMessage:
+    async def __handle_message_peer_unavailable(
+        self, message: CansMessage
+    ) -> None:
+        """Handle message type PEER_UNAVAILABLE."""
+        peer = message.payload["peer"]
+        print(f"Peer {peer} unavailable!")
+
+    async def __handle_message_peer_login(self, message: CansMessage) -> None:
+        """Handle message type PEER_LOGIN."""
+        peer = message.payload["peer"]
+        print(f"Peer {peer} just logged in!")
+
+    async def __handle_message_peer_logout(self, message: CansMessage) -> None:
+        """Handle message type PEER_LOGOUT."""
+        peer = message.payload["peer"]
+        print(f"Peer {peer} just logged out!")
+
+    async def __handle_message_active_friends(
+        self, message: CansMessage
+    ) -> None:
+        """Handle message type ACTIVE_FRIENDS."""
+        friends = message.payload["friends"]
+        print(f"Active friends: {friends}")
+
+    def __user_message_to(self, peer: PubKeyDigest) -> UserMessage:
         """Create a user message to a peer."""
         message = UserMessage(peer)
         message.header.sender = self.key_manager.get_own_public_key_digest()
--- server/database_manager_server/__init__.py
+++ server/database_manager_server/__init__.py
@@ -5,6 +5,9 @@ fetching/committing application-specific data.
 """
 
 import logging
+from typing import Dict, Set
+
+from common.keys import PubKeyDigest
 
 
 class DatabaseManager:
@@ -13,3 +16,24 @@ class DatabaseManager:
     def __init__(self) -> None:
         """Construct the serverside database manager."""
         self.log = logging.getLogger("cans-logger")
+        self.subscribers_of: Dict[PubKeyDigest, Set[PubKeyDigest]] = {}
+
+    async def get_subscribers_of(
+        self, target: PubKeyDigest
+    ) -> Set[PubKeyDigest]:
+        """Fetch a list of subscribers."""
+        # TODO: Use the database
+        if target in self.subscribers_of.keys():
+            return self.subscribers_of[target]
+        else:
+            return set()
+
+    async def add_subscriber_of(
+        self, target: PubKeyDigest, subscriber: PubKeyDigest
+    ) -> None:
+        """Store a subscription in the database."""
+        # TODO: Use the database
+        if target in self.subscribers_of.keys():
+            self.subscribers_of[target].add(subscriber)
+        else:
+            self.subscribers_of[target] = {subscriber}
