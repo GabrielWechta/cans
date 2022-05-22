db76a35546fb2442424c2c061f5cbbcec71c9fbb
Oliwer Sobolewski <247291@student.pwr.edu.pl>
2022-05-23 01:30:20 +0200

    Define workable database API

--- client/src/client/user_interface/__main__.py
+++ client/src/client/user_interface/__main__.py
@@ -1,55 +1,60 @@
 """For CLI usage and debugging."""
-import asyncio
-import hashlib
+# import asyncio
+# import datetime
+# import hashlib
 
-from blessed import Terminal
+# from blessed import Terminal
 
-from ..models import MessageModel, UserModel
-from ..user_interface import UserInterface
+# from ..database_manager_client import DatabaseManager
+# from ..models import Friend, Message, MessageModel, UserModel
+# from ..user_interface import UserInterface
 
 # from .view import View
 
 
-async def upstream_sink(message_model: MessageModel) -> None:
-    """Drop outgoing messages."""
-    assert message_model
+# async def upstream_sink(message_model: Message) -> None:
+#    """Drop outgoing messages."""
+#    assert message_model
 
 
-term = Terminal()
-loop = asyncio.get_event_loop()
+# term = Terminal()
+# loop = asyncio.get_event_loop()
+# db_manager = DatabaseManager("test.db", "132")
+# db_manager.initialize()
 
-ui = UserInterface(
-    loop=loop,
-    upstream_callback=upstream_sink,
-    identity=UserModel(username="Alice", id="13", color="green"),
-)
+# ui = UserInterface(
+#     loop=loop,
+#     upstream_callback=upstream_sink,
+#     identity=Friend(username="Alice", id="13", color="green"),
+#     db_manager=db_manager,
+# )
 
-eve = UserModel(
-    username="Eve",
-    id=hashlib.md5("ee".encode("utf-8")).hexdigest(),
-    color="blue",
-)
+# eve = Friend(
+#     username="Eve",
+#     id=hashlib.md5("ee".encode("utf-8")).hexdigest(),
+#     color="blue",
+# )
 
-bob = UserModel(
-    username="Bob",
-    id=hashlib.md5("aa".encode("utf-8")).hexdigest(),
-    color="red",
-)
+# bob = Friend(
+#     username="Bob",
+#     id=hashlib.md5("aa".encode("utf-8")).hexdigest(),
+#     color="red",
+# )
 
-ui.view.add_chat(bob)
-ui.view.add_chat(eve)
-ui.view.add_chat(bob)
+# ui.view.add_chat(bob)
+# ui.view.add_chat(eve)
+# ui.view.add_chat(bob)
 
-loop.create_task(ui.view.render_all())
-
-
-async def send_test_messages_from_bob() -> None:
-    """Send some stuff from bob."""
-    while True:
-        ui.on_new_message_received("test message", bob)
-        await asyncio.sleep(2)
+# loop.create_task(ui.view.render_all())
 
 
-loop.create_task(send_test_messages_from_bob())
+# async def send_test_messages_from_bob() -> None:
+#     """Send some stuff from bob."""
+#     while True:
+#         ui.on_new_message_received("test message", bob)
+#         await asyncio.sleep(2)
 
-loop.run_forever()
+
+# loop.create_task(send_test_messages_from_bob())
+
+# loop.run_forever()
--- client/src/client/user_interface/view.py
+++ client/src/client/user_interface/view.py
@@ -5,7 +5,6 @@ Divides terminal into independent windows
 """
 import asyncio
 import concurrent
-import hashlib
 import logging
 import signal
 from datetime import datetime
@@ -14,7 +13,8 @@ from typing import Any, Callable, List, Mapping, Union
 
 from blessed import Terminal
 
-from ..models import MessageModel, UserModel
+from ..database_manager_client import DatabaseManager
+from ..models import Friend, Message
 from .tiles import ChatTile, HeaderTile, InputTile
 from .tiling_managers import MonadTallLayout
 
@@ -30,11 +30,13 @@ class View:
         self,
         term: Terminal,
         loop: Union[asyncio.BaseEventLoop, asyncio.AbstractEventLoop],
-        identity: UserModel,
+        identity: Friend,
+        db_manager: DatabaseManager,
     ) -> None:
         """Instantiate a view."""
         self.term = Terminal()
         self.loop = loop
+        self.db_manager = db_manager
 
         self.log = logging.getLogger("cans-logger")
 
@@ -97,158 +99,19 @@ class View:
         # render the screen
         loop.run_until_complete(self.render_all())
 
-    def get_message_history(self, user: UserModel) -> List[MessageModel]:
+    def get_message_history(self, user: Friend) -> List[Message]:
         """Get message history for a given user."""
-        # TODO: do it from DB
-        eve = UserModel(
-            username="Eve",
-            id=hashlib.md5("ee".encode("utf-8")).hexdigest(),
-            color="blue",
-        )
+        messages = self.db_manager.get_message_history_with_friend(user.id)
 
-        bob = UserModel(
-            username="Bob",
-            id=hashlib.md5("aa".encode("utf-8")).hexdigest(),
-            color="red",
-        )
-
-        messages_with_bob = [
-            MessageModel(
-                from_user=self.myself,
-                to_user=bob,
-                body="don't know... let's try:",
-                date=datetime(
-                    year=2022, month=3, day=21, hour=11, minute=36, second=15
-                ),
-            ),
-            MessageModel(
-                to_user=self.myself,
-                from_user=bob,
-                body=f"Okay that's { self.term.lightgreen_bold('cool') }, "
-                "but do we have markdown support?",
-                date=datetime(
-                    year=2022, month=3, day=21, hour=11, minute=36, second=15
-                ),
-            ),
-            MessageModel(
-                from_user=self.myself,
-                to_user=bob,
-                body=self.term.blue_underline(
-                    "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-                ),
-                date=datetime(
-                    year=2022, month=3, day=21, hour=11, minute=36, second=15
-                ),
-            ),
-            MessageModel(
-                from_user=self.myself,
-                to_user=bob,
-                body="Observe",
-                date=datetime(
-                    year=2022, month=3, day=21, hour=11, minute=36, second=15
-                ),
-            ),
-            MessageModel(
-                from_user=bob,
-                to_user=self.myself,
-                body="No way dude",
-                date=datetime(
-                    year=2022, month=3, day=21, hour=11, minute=36, second=15
-                ),
-            ),
-            MessageModel(
-                from_user=self.myself,
-                to_user=bob,
-                body="You know we can post links here?",
-                date=datetime(
-                    year=2022, month=3, day=21, hour=11, minute=36, second=15
-                ),
-            ),
-            MessageModel(
-                from_user=bob,
-                to_user=self.myself,
-                body="What do you want",
-                date=datetime(
-                    year=2022, month=3, day=21, hour=11, minute=36, second=15
-                ),
-            ),
-            MessageModel(
-                from_user=self.myself,
-                to_user=bob,
-                body="Hi",
-                date=datetime(
-                    year=2022, month=3, day=21, hour=11, minute=36, second=15
-                ),
-            ),
-            MessageModel(
-                from_user=bob,
-                to_user=self.myself,
-                body="Hello",
-                date=datetime(
-                    year=2022, month=3, day=21, hour=11, minute=36, second=15
-                ),
-            ),
-        ]
-
-        messages_with_eve = [
-            MessageModel(
-                from_user=self.myself,
-                to_user=eve,
-                body="don't know... let's try:",
-                date=datetime(
-                    year=2022, month=3, day=21, hour=11, minute=36, second=15
-                ),
-            ),
-            MessageModel(
-                from_user=eve,
-                to_user=self.myself,
-                body="hello there",
-                date=datetime(
-                    year=2022, month=3, day=21, hour=11, minute=36, second=15
-                ),
-            ),
-            MessageModel(
-                from_user=self.myself,
-                to_user=eve,
-                body="heya",
-                date=datetime(
-                    year=2022, month=3, day=21, hour=11, minute=36, second=15
-                ),
-            ),
-        ]
-
-        messages = {
-            "bob": messages_with_bob,
-            "eve": messages_with_eve,
-        }
-
-        if user.username.lower() in messages:
-            return messages[user.username.lower()]
+        if messages:
+            messages.reverse()
+            return messages
         else:
             return []
 
-    def get_friends(self) -> Mapping[str, UserModel]:
-        """Get list of friends from DB as username:UserModel dict."""
-        # TODO: do it from DB
-        eve = UserModel(
-            username="Eve",
-            id=hashlib.md5("ee".encode("utf-8")).hexdigest(),
-            color="blue",
-        )
-
-        bob = UserModel(
-            username="Bob",
-            id=hashlib.md5("aa".encode("utf-8")).hexdigest(),
-            color="red",
-        )
-
-        jeremiah = UserModel(
-            username="Jeremiah",
-            id=hashlib.md5("jj".encode("utf-8")).hexdigest(),
-            color="pink",
-        )
-
-        friends = [eve, bob, jeremiah]
+    def get_friends(self) -> Mapping[str, Friend]:
+        """Get list of friends from DB as username:Friend dict."""
+        friends = self.db_manager.get_all_friends()
 
         friends_dict = {}
         for friend in friends:
@@ -282,7 +145,7 @@ class View:
         self.loop.create_task(self.header.render(self.term))
         self.on_resize_event.set()
 
-    def add_chat(self, chat_with: Union[UserModel, str]) -> None:
+    def add_chat(self, chat_with: Union[Friend, str]) -> None:
         """Add a chat tile with a given user."""
         # TODO: get it from DB
         if isinstance(chat_with, str):
@@ -308,7 +171,7 @@ class View:
         self.layout.add(chat)
         # self.loop.create_task(self.layout.render_all())
 
-    def swap_chat(self, chat_with: Union[UserModel, str]) -> None:
+    def swap_chat(self, chat_with: Union[Friend, str]) -> None:
         """Add swap current tile with a new chat with given user."""
         tile_before = self.layout.current_tile
 
@@ -339,12 +202,12 @@ class View:
         # delete the old tile
         self.layout.remove(tile_before)
 
-    def find_chats(self, chats_with: Union[UserModel, str]) -> List[ChatTile]:
+    def find_chats(self, chats_with: Union[Friend, str]) -> List[ChatTile]:
         """Find chat tiles with a given user or user id."""
         found_tiles = []
 
-        # if UserModel is provided
-        if isinstance(chats_with, UserModel):
+        # if Friend is provided
+        if isinstance(chats_with, Friend):
             user_id = chats_with.id
         # if string is provided
         else:
@@ -359,18 +222,18 @@ class View:
 
     def add_message(
         self,
-        chat_with: Union[UserModel, str],
-        message: Union[MessageModel, str],
+        chat_with: Union[Friend, str],
+        message: Union[Message, str],
     ) -> None:
         """Add a message to buffers of chat tiles with given user of id."""
         chats = self.find_chats(chat_with)
         if len(chats) > 0:
-            # if we're provided with a MessageModel, just use it
-            if isinstance(message, MessageModel):
+            # if we're provided with a Message, just use it
+            if isinstance(message, Message):
                 new_message = message
             # else we only have payload, so we have to construct it
             else:
-                new_message = MessageModel(
+                new_message = Message(
                     from_user=chats[0].chat_with,  # type: ignore
                     to_user=self.myself,
                     body=message,  # type: ignore
@@ -396,145 +259,3 @@ class View:
 if __name__ == "__main__":
     term = Terminal()
     loop = asyncio.get_event_loop()
-
-    view = View(
-        term=term,
-        loop=loop,
-        identity=UserModel(username="Alice", id="13", color="green"),
-    )
-
-    eve = UserModel(
-        username="Eve",
-        id=hashlib.md5("ee".encode("utf-8")).hexdigest(),
-        color="blue",
-    )
-
-    bob = UserModel(
-        username="Bob",
-        id=hashlib.md5("aa".encode("utf-8")).hexdigest(),
-        color="red",
-    )
-    view.add_chat(bob)
-
-    # create chat tile with eve
-    view.add_chat(eve)
-    view.add_chat(eve)
-    view.add_chat(eve)
-    view.add_chat(eve)
-
-    chats_with_bob = view.find_chats(bob)
-    chats_with_eve = view.find_chats(eve)
-
-    messages_with_bob = [
-        MessageModel(
-            from_user=view.myself,
-            to_user=bob,
-            body="don't know... let's try:",
-            date=datetime(
-                year=2022, month=3, day=21, hour=11, minute=36, second=15
-            ),
-        ),
-        MessageModel(
-            to_user=view.myself,
-            from_user=bob,
-            body=f"Okay that's { term.lightgreen_bold('cool') }, "
-            "but do we have markdown support?",
-            date=datetime(
-                year=2022, month=3, day=21, hour=11, minute=36, second=15
-            ),
-        ),
-        MessageModel(
-            from_user=view.myself,
-            to_user=bob,
-            body=term.blue_underline(
-                "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-            ),
-            date=datetime(
-                year=2022, month=3, day=21, hour=11, minute=36, second=15
-            ),
-        ),
-        MessageModel(
-            from_user=view.myself,
-            to_user=bob,
-            body="Observe",
-            date=datetime(
-                year=2022, month=3, day=21, hour=11, minute=36, second=15
-            ),
-        ),
-        MessageModel(
-            from_user=bob,
-            to_user=view.myself,
-            body="No way dude",
-            date=datetime(
-                year=2022, month=3, day=21, hour=11, minute=36, second=15
-            ),
-        ),
-        MessageModel(
-            from_user=view.myself,
-            to_user=bob,
-            body="You know we can post links here?",
-            date=datetime(
-                year=2022, month=3, day=21, hour=11, minute=36, second=15
-            ),
-        ),
-        MessageModel(
-            from_user=bob,
-            to_user=view.myself,
-            body="What do you want",
-            date=datetime(
-                year=2022, month=3, day=21, hour=11, minute=36, second=15
-            ),
-        ),
-        MessageModel(
-            from_user=view.myself,
-            to_user=bob,
-            body="Hi",
-            date=datetime(
-                year=2022, month=3, day=21, hour=11, minute=36, second=15
-            ),
-        ),
-        MessageModel(
-            from_user=bob,
-            to_user=view.myself,
-            body="Hello",
-            date=datetime(
-                year=2022, month=3, day=21, hour=11, minute=36, second=15
-            ),
-        ),
-    ]
-
-    messages_with_eve = [
-        MessageModel(
-            from_user=view.myself,
-            to_user=eve,
-            body="don't know... let's try:",
-            date=datetime(
-                year=2022, month=3, day=21, hour=11, minute=36, second=15
-            ),
-        ),
-        MessageModel(
-            from_user=eve,
-            to_user=view.myself,
-            body="hello there",
-            date=datetime(
-                year=2022, month=3, day=21, hour=11, minute=36, second=15
-            ),
-        ),
-        MessageModel(
-            from_user=view.myself,
-            to_user=eve,
-            body="heya",
-            date=datetime(
-                year=2022, month=3, day=21, hour=11, minute=36, second=15
-            ),
-        ),
-    ]
-
-    messages_with_bob.reverse()
-    for message in messages_with_bob:
-        view.add_message(bob, message)
-
-    for message in messages_with_eve:
-        view.add_message(eve, message)
-
-    loop.run_forever()
